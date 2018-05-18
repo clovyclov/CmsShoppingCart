ff07ac0dcf96633adf137b98987e443d7b2e3f19
Clovis Shropshire <shropshireclovis@gmail.com>
2018-05-17 22:04:47 -0400

    Created Admin Dashboard controller and view

--- CmsShoppingCart/App_Start/BundleConfig.cs
+++ CmsShoppingCart/App_Start/BundleConfig.cs
@@ -8,8 +8,13 @@ namespace CmsShoppingCart
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
